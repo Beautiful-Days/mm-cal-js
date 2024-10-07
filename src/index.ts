ffc0bdf3427c75058f864ba9f1125b58c59b5534
ninnroot <averyformalname@gmail.com>
2024-10-07 17:11:15 +0800

    struggled with npm publication

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { julianToMyanmarDate } from "./mm-calendar/julian-to-myanmar";
 import { getMyanmarDate } from "./mm-calendar/myanmar-date";
 import { westernToJulianDayNumber } from "./mm-calendar/western-to-julian";
 
-console.log(julianToMyanmarDate(2459946))
-
-console.log(isSabbath(new Date(2024,9,17)))
+export {
+    isSabbath,
+    julianToMyanmarDate,
+    getMyanmarDate,
+    westernToJulianDayNumber
+}
