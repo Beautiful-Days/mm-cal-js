f2ef87a725815f125df6fdbf93a729291106b24d
Pho Thin Maung <101423229+phothinmg@users.noreply.github.com>
2024-10-12 13:45:32 +0630

    Delete a.ts

--- a.ts
+++
@@ -1,4 +0,0 @@
-import { publicHolidays, holidays } from "./src/mm-calendar/holidays";
-
-console.log(publicHolidays(new Date(2021, 3, 13)));
-console.log(holidays.thingyanHolidays(2459318, 1382, 1));
